35bd1bb7065baebd7a80daae7501dfe715606d07
Daniel009891 <>
2023-12-06 21:31:04 +0000

    Change allowed hosts url

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codestar0098.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['codestar0098-9a9c2d48510c.herokuapp.com', 'localhost']
 
 
 # Application definition
