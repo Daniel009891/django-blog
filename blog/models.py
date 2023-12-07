5051697ac1ca046ec38652791739a57e8d501c46
Daniel009891 <>
2023-12-07 18:29:43 +0000

    Add authentication for accounts sign up

--- blog/models.py
+++ blog/models.py
@@ -24,8 +24,10 @@ class Post(models.Model):
 class Meta:
     ordering = ["-created_on"]
 
+
 def __str__(self):
     return self.title
+
 
 def number_of_likes(self):
     return self.likes.count()
